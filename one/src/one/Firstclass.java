0439a754d7d5582fc81a97cf4fec6e6c9a33938d
VASANTH S <s.vasanthakumar1112@gmail.com>
2022-01-26 13:27:31 +0530

    First Commit

---
+++ one/src/one/Firstclass.java
@@ -0,0 +1,13 @@
+package one;
+
+public class Firstclass {
+
+	public Firstclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Vasanth!");
+	}
+}
